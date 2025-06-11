d6ed26298d5bb8d40d9240d2b855e6d9ce21514a
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2025-06-11 20:21:23 +0300

    Add Bash script for installing Docker, Docker Compose, Python, and Django

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -13,28 +13,28 @@ if ! which docker &> /dev/null; then
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt-get update
-  apt-get install docker-ce docker-ce-cli containerd.io
+  apt-get install -y docker-ce docker-ce-cli containerd.io
   else
     echo "Docker is already installed"
 fi
 
 # install docker compose
 if ! which docker-compose &> /dev/null; then
-  apt-get install docker-compose
+  apt-get install -y docker-compose
   else
     echo "Docker compose is already installed"
 fi
 
 # install python 3.9
 if ! which python3.9 &> /dev/null; then
-  apt-get install python3.9
+  apt-get install -y python3.9
   else
     echo "Python 3.9 is already installed"
 fi
 
 # install django
 if ! which django-admin &> /dev/null; then
-  pip install django
+  pip install -y django
   else
     echo "Django is already installed"
 fi
