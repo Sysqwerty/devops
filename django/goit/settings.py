4a83b608cfe28c8d8b50d75a17375c5a67440cff
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2025-07-06 12:42:08 +0300

    update Django-app settings allowed_hosts

--- django/goit/settings.py
+++ django/goit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-27lopov)zh#-mz)0jkj64-&r#@*sjh!@yof^gystce^5nufu(l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
