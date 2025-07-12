54079a4e8b83bdf8146e248bc3598ee74945cf8f
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2025-07-12 18:57:38 +0300

    update creds usage 4

--- modules/jenkins/jenkins.tf
+++ modules/jenkins/jenkins.tf
@@ -79,6 +79,15 @@ resource "aws_iam_role_policy" "jenkins_ecr_policy" {
   })
 }
 
+# ----------  Helm release  ----------
+locals {
+  jenkins_values = templatefile("${path.module}/values.yaml.tmpl", {
+    github_user     = var.github_user
+    github_pat      = var.github_pat        # <<< sensitive flows in here
+    github_repo_url = var.github_repo_url
+  })
+}
+
 resource "helm_release" "jenkins" {
   name             = "jenkins"
   namespace        = "jenkins"
@@ -87,12 +96,9 @@ resource "helm_release" "jenkins" {
   version          = "5.8.27"
   create_namespace = false
 
-  values = [
-    templatefile("${path.module}/values.yaml.tmpl", {
-      github_user     = var.github_user
-      github_pat      = var.github_pat      # << sensitive var flows in here
-      github_repo_url = var.github_repo_url
-    })
-  ]
+  values  = [local.jenkins_values]
+
+  # give Jenkins up to 10 min to become Ready (default is only 5 min)
+  timeout = 600
 }
 
--- modules/jenkins/providers.tf
+++ modules/jenkins/providers.tf
@@ -1,16 +1,7 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.0.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
+    aws        = { source = "hashicorp/aws",  version = "~> 5.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.23" }
+    helm       = { source = "hashicorp/helm", version = "~> 2.12" }
   }
 }
