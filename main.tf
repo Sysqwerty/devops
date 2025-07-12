ffa1f19c1fda467d4b6ba1d5f7a7b522615bf6c6
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2025-07-12 19:18:34 +0300

    update creds usage 6

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
 terraform {
   required_providers {
-    aws        = { source = "hashicorp/aws",  version = "~> 5.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.23" }
-    helm       = { source = "hashicorp/helm", version = "~> 2.12" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
   }
 }
-
 
 provider "aws" {
   region = var.region
@@ -63,7 +71,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
--- modules/jenkins/providers.tf
+++ modules/jenkins/providers.tf
@@ -1,7 +1,16 @@
 terraform {
   required_providers {
-    aws        = { source = "hashicorp/aws",  version = "~> 5.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.23" }
-    helm       = { source = "hashicorp/helm", version = "~> 2.12" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
   }
 }
