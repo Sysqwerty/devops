b649bd7c1c8eb03af2996d8663c70c3208a7633a
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2025-07-12 18:01:13 +0300

    update creds usage 2

--- modules/jenkins/jenkins.tf
+++ modules/jenkins/jenkins.tf
@@ -87,6 +87,12 @@ resource "helm_release" "jenkins" {
   version          = "5.8.27"
   create_namespace = false
 
-  values = [file("${path.module}/values.yaml")]
+  values = [
+    templatefile("${path.module}/values.yaml.tmpl", {
+      github_user     = var.github_user
+      github_pat      = var.github_pat      # << sensitive var flows in here
+      github_repo_url = var.github_repo_url
+    })
+  ]
 }
 
