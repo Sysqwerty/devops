c64c764c6422f4a0fa69e8349e5bafb69b21cb27
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2025-07-13 18:35:57 +0300

    Revert "update creds usage 7"
    This reverts commit e3819b329a12fee91831b59885e10187466e18be.

--- outputs.tf
+++ outputs.tf
@@ -93,11 +93,6 @@ output "jenkins_namespace" {
   value       = module.jenkins.jenkins_namespace
 }
 
-output "github_username" {
-  description = "GitHub username"
-  value       = var.github_user
-}
-
 #-------------ArgoCD-----------------
 
 output "argocd_namespace" {
